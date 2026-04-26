c4d6e30f37c4093948363585f38e1a486792f808
Gábor Lipták <shearer83@gmail.com>
2026-04-26 15:01:20 +0200

    refactor: clean root __init__.py

--- src/apaleoapi/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Apaleo API Client - Python wrapper for Apaleo Swagger API."""
-
-__version__ = "0.1.0"
-__author__ = "Gábor Lipták"
-__email__ = "shearer83@gmail.com"
-
-from .client import ApaleoAPIClient
-
-__all__ = ["ApaleoAPIClient", "__version__"]
